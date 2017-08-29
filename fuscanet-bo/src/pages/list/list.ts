5b4d35a17e3534f8fd12dae1d310bfda96648fb3
codigo <omargonzaleztecheira@gmail.com>
2017-08-29 17:38:47 -0300

    listado de perfiles

--- fuscanet-bo/src/pages/list/list.ts
+++ fuscanet-bo/src/pages/list/list.ts
@@ -3,7 +3,7 @@ import { IonicPage, NavController, NavParams, AlertController,Platform } from 'i
 import firebase from 'firebase';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Profile } from '../../models/profile';
-import { AngularFireDatabase, FirebaseObjectObservable  } from 'angularfire2/database';
+import { AngularFireDatabase, FirebaseListObservable  } from 'angularfire2/database';
 
 @Component({
   selector: 'page-list',
@@ -12,24 +12,15 @@ import { AngularFireDatabase, FirebaseObjectObservable  } from 'angularfire2/dat
 export class ListPage {
   user={};
   role={};
-  allProfiles=[];
+  allProfiles$: FirebaseListObservable<Profile[]>;
 
   constructor(private afDb: AngularFireDatabase,private afAuth:AngularFireAuth,public navCtrl: NavController, public navParams: NavParams) {
-    
-  }
-  ionViewDidLoad() {
-    this.afAuth.authState.subscribe(data => {
+      this.afAuth.authState.subscribe(data => {
       this.user = data;
       //console.log(this.user);  
-      this.afDb.list(`/profile/`).subscribe(_data => {
-          //aca tengo todos los perfiles
-          //tengo que cotejar que filtrar solo los q no tienen role
-          //encarar los observables......
-          this.allProfiles = _data;
-          console.log(this.allProfiles);
-      });  
-     });
-    
+        this.allProfiles$ = this.afDb .list('profile');
+        //this.allProfiles$.subscribe(data => console.log(data));
+     }); 
   }
 
 }
