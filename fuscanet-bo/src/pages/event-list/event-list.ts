5b53829581aabeceb45b8c82d8c3660a26649366
codigo <omargonzaleztecheira@gmail.com>
2018-02-07 16:17:37 -0300

    filtro de listado de eventos

--- fuscanet-bo/src/pages/event-list/event-list.ts
+++ fuscanet-bo/src/pages/event-list/event-list.ts
@@ -12,20 +12,37 @@ import { AngularFireDatabase, FirebaseListObservable  } from 'angularfire2/datab
 export class EventList {
   user={};
   allEvents$: FirebaseListObservable<Profile[]>;
+  eventToShow$:any ;
+  origEvent:any;
 
   constructor(public alertCtrl: AlertController,private afDb: AngularFireDatabase,private afAuth:AngularFireAuth,public navCtrl: NavController, public navParams: NavParams) {
     this.afAuth.authState.subscribe(data => {
     this.user = data;
     //console.log(this.user);  
     this.allEvents$ = this.afDb.list('events');
-    
+    this.allEvents$.subscribe(data =>{
+      this.origEvent = data;
+    })
     //this.allProfiles$.subscribe(data => console.log(data));
 
    }); 
 }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad EventList');
+  getItems(ev: any) {
+    // Reset items back to all of the items
+    this.eventToShow$ = this.origEvent;
+
+    // set val to the value of the searchbar
+    let val = ev.target.value;
+    //console.log(this.eventToShow$);
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.eventToShow$ = this.eventToShow$.filter((item) => {
+        //console.log(item.nombre);
+        //console.log(val);
+        return (item.title.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
   }
 
 }
