64c01f279a9225ca8ee54507709a17b479350438
codigo <omargonzaleztecheira@gmail.com>
2018-02-08 15:50:46 -0300

    filtro en el listado de convenios

--- fuscanet-bo/src/pages/convenio-list/convenio-list.ts
+++ fuscanet-bo/src/pages/convenio-list/convenio-list.ts
@@ -12,20 +12,35 @@ import { AngularFireDatabase, FirebaseListObservable  } from 'angularfire2/datab
 export class ConvenioList {
   user={};
   allConvenios$: FirebaseListObservable<Profile[]>;
+  conveniosToShow$:any;
+  origConvenios:any;
 
   constructor(public alertCtrl: AlertController,private afDb: AngularFireDatabase,private afAuth:AngularFireAuth,public navCtrl: NavController, public navParams: NavParams) {
     this.afAuth.authState.subscribe(data => {
     this.user = data;
-    //console.log(this.user);  
     this.allConvenios$ = this.afDb.list('convenio');
-    
-    //this.allProfiles$.subscribe(data => console.log(data));
-
+    this.allConvenios$.subscribe(data =>{
+      this.origConvenios = data;
+      this.conveniosToShow$ = this.origConvenios;
+    })
    }); 
 }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad EventList');
+getItems(ev: any) {
+  // Reset items back to all of the items
+  this.conveniosToShow$ = this.origConvenios;
+
+  // set val to the value of the searchbar
+  let val = ev.target.value;
+  //console.log(this.eventToShow$);
+  // if the value is an empty string don't filter the items
+  if (val && val.trim() != '') {
+    this.conveniosToShow$ = this.conveniosToShow$.filter((item) => {
+      //console.log(item.nombre);
+      //console.log(val);
+      return (item.title.toLowerCase().indexOf(val.toLowerCase()) > -1);
+    })
   }
+}
 
 }
