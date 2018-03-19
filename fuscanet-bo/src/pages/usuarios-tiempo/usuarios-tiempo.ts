cb0f8d090a6f72e23fef0966a32fb057d74232e3
codigo <omargonzaleztecheira@gmail.com>
2018-03-19 15:52:05 -0300

    filtrar usuarios registrados por rango de fechas

--- fuscanet-bo/src/pages/usuarios-tiempo/usuarios-tiempo.ts
+++ fuscanet-bo/src/pages/usuarios-tiempo/usuarios-tiempo.ts
@@ -15,13 +15,28 @@ export class UsuariosTiempo {
   allProfiles$: FirebaseListObservable<Profile[]>;
   profilesToShow$:any;
   origProfiles:any;
+  startTime:any;
+  endTime:any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,private afDb: AngularFireDatabase,private afAuth:AngularFireAuth) {
     this.allProfiles$ = this.afDb.list('profile');
     this.allProfiles$.subscribe(data => {
       this.origProfiles = data;
-      this.profilesToShow$ = this.origProfiles;  
+      //this.profilesToShow$ = this.origProfiles;  
     });
+  }
+  filtrar(){
+    this.profilesToShow$=[];
+    if(!this.startTime || !this.endTime){
+      alert('Complete los campos correctamente para realizar la busqueda');
+    }else{
+      this.origProfiles.forEach(element => {
+        console.log(element.created);
+        if(element.created > this.startTime && element.created < this.endTime){
+          this.profilesToShow$.push(element);
+        }
+      });
+    }
   }
 
 }
